310b91512a31624fa98980a2c6f910a15d17e869
I569996 <maximilian.otto@sap.com>
2025-06-26 11:44:17 +0200

    overview data

---
+++ dashboard.py
@@ -0,0 +1,49 @@
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
+from sklearn import datasets
+from sklearn.tree import DecisionTreeClassifier, export_text
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+import plotly.graph_objs as go
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
+
+# Dashboard im Browser anzeigen
+# http://127.0.0.1:8050/
+
+df = pd.read_csv("data/smoker_train.csv")
+
+# load images
+def encode_image(image_file):
+    with open(image_file, 'rb') as f:
+        encoded = base64.b64encode(f.read()).decode()
+    return f"data:image/jpeg;base64,{encoded}"
+
+tree = encode_image("images/dashboard_tree.jpeg")
+weight_height = encode_image("images/dashboard_weight_height.png")
+age = encode_image("images/dashboard_age.png")
+smoker = encode_image("images/dashboard_smoker.png")
+
+
+# Dash-Anwendung initialisieren
+app = dash.Dash(__name__)
+
+# Layout des Dash-Dashboards definieren
+app.layout = html.Div([
+    html.Div([
+        html.H3("Data Overview"),
+        html.Div([
+            html.Img(src=weight_height, style={'width': '400px', 'height': 'auto'}),
+            html.Img(src=smoker, style={'width': '400px', 'height': 'auto'}),
+            html.Img(src=age, style={'width': '400px', 'height': 'auto'})
+        ], style={'display': 'flex', 'justifyContent': 'space-between', 'gap': '20px'})
+    ])
+])
+
+# Dash-Anwendung ausführen
+if __name__ == '__main__':
+    app.run(debug=True) # Debug Modus für Ausgabe von Fehlermeldungen im Dashboard uvm
